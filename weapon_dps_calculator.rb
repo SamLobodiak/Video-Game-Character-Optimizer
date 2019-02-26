fdc47a843547b0af60c7c05976c5b04914588203
Samuel <samuel.lobodiak@gmail.com>
2019-02-26 15:22:21 -0600

    Add greatsword weapon object and swings per stamina bar attribute

--- weapon_dps_calculator.rb
+++ weapon_dps_calculator.rb
@@ -1,16 +1,23 @@
-weapons = {
+	player = {
+		health: 250,
+		stamina: 100,
+	}
+	weapons = {
 
-  longsword: { damage: 80, swing_speed: 0.5, length: 3,   stamina_cost: 25, weight: 4},
-  
-  dagger: {damage: 56, swing_speed: 0.25, length: 1, stamina_cost: 7, weight: 1},
-}
+	  longsword: {damage: 80, swing_speed: 0.5, length: 3,   stamina_cost: 15, weight: 3},
+	  dagger: {damage: 56, swing_speed: 0.25, length: 1, stamina_cost: 7, weight: 0.5},
+	  greatsword: {damage: 130, swing_speed: 0.8, length: 5, stamina_cost: 33, weight: 12},
+	}
 
 
-weapons_damage_per_second = Hash.new
+	weapon_outputs = Hash.new
 
-weapons.each do |key, value|
-  #pushing these results onto the new hash
-  weapons_damage_per_second[key] = value[:damage]/value[:swing_speed]
-end
+	weapons.each do |key, value|
+	  #Creating a new hash with weapon information
+	  weapon_outputs[key] = {
+	  	damage_per_second: value[:damage]/value[:swing_speed],
+	  	total_swings_per_stamina_bar: player[:stamina]/value[:stamina_cost]
+	  }
+	end
 
-puts weapons_damage_per_second
+	puts weapon_outputs
