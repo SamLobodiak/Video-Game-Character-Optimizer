1231975a86d08a5e256b5a4bb9a3328af3574805
Samuel <samuel.lobodiak@gmail.com>
2019-03-08 15:47:36 -0600

    Fix 0 and nil issues in raw weapon data, add onto the weapons_max hash

--- weapon_analyzer.rb
+++ weapon_analyzer.rb
@@ -1,3 +1,14 @@
+
+####Some of the raw weapon data 'weight' has 0 or nil values.  This creates conflicts.  To solve this I have to.
+# 1. Give the 0 or nil values a value of '0.5' (a float) which is close to the actual weight in the video game.
+
+for x in weapons
+  if x[:weight] == nil || x[:weight] < 1
+    x[:weight] = 0.5
+  end
+end
+#############################
+
 player = {
   health: 250.0,
   stamina: 100.0,
@@ -9,46 +20,57 @@ weapons_output = Hash.new
 temp = 0
 
 #from the raw data, creating a hash of weapon names with their damage characteristics
-
 for x in weapons
+  weight = x[:weight]
   # <=25% of equip load yields fast roll, >25% to <=50% yields medium roll, >50% to <=75% yields slow roll
   leftover_weight_for_armour_to_fast_roll = player[:equip_max] * 0.25 - x[:weight].to_f
   leftover_weight_for_armour_to_med_roll = player[:equip_max] * 0.50 - x[:weight].to_f
   leftover_weight_for_armour_to_fat_roll = player[:equip_max] * 1.00 - x[:weight].to_f
-  #reqs_sum is the sum of the integers of strength, dexterity, faith, and intelligence required to wield the weapon
-  reqs_sum = 0
+  #pre_reqs_sum is the sum of the integers of strength, dexterity, faith, and intelligence required to wield the weapon
+  pre_reqs_sum = 0
   #Of this weapon, iterate through the stat requirements for it
   for y in x[:req]
-    reqs_sum = reqs_sum + y[1]
+    pre_reqs_sum = pre_reqs_sum + y[1]
   end
+  
+  # Overall damage is combined physical and elemental damage if there is any
+  overall_damage = 0
+  for key, value in x[:atk]
+    combined_damage = overall_damage + value
+  end
+
+  dmg_per_weight = combined_damage / weight
   #Outputting all of the data onto an output hash
-  weapons_output.merge!(x[:name] => {"atk" => x[:atk], "weight" => x[:weight], "reqs_sum" => reqs_sum, "to_fast_roll" => leftover_weight_for_armour_to_fast_roll, "to_med_roll" => leftover_weight_for_armour_to_med_roll, "to_slow_roll" => leftover_weight_for_armour_to_fat_roll})
+  weapons_output.merge!(x[:name] => {"atk" => x[:atk], "combined_damage" => combined_damage, "dmg_per_weight" => dmg_per_weight, "weight" => weight, "pre_reqs_sum" => pre_reqs_sum, "to_fast_roll" => leftover_weight_for_armour_to_fast_roll, "to_med_roll" => leftover_weight_for_armour_to_med_roll, "to_slow_roll" => leftover_weight_for_armour_to_fat_roll})
 end
 
+# Running find max algorithms on all of the weapons
 weapons_max = Hash.new
 max_damage = 0
 max_bonus = 0
 max_magic = 0
-# This code block runs find max algorithms on the weapons and puts the maxes on a new hash call 'weapons_max'
 for key, value in weapons_output
   #Max physical damage onto a hash
-  if value["atk"][:physical] > max_damage
+  if value["atk"][:physical] > max_damage  
     max_damage = value["atk"][:physical]
-    weapons_max.merge!("highest_damage_physical" => { "name" => key, "amount" => max_damage})
+    weapons_max.merge!("highest_damage_physical" => { "name" => key, "amount" => max_damage}) 
   end
     #Max magic damage and putting it onto hash
   if value["atk"][:magic] > max_magic
-    max_magic = value["atk"][:magic]
+    max_magic = value["atk"][:magic]    
     weapons_max.merge!("highest_damage_magic" => {"name" => key, "amount" => max_magic})
   end
+
   #Max bonus (critical hit multiplier) and putting it onto a hash
   if value["atk"][:bonus] > max_bonus
-    max_bonus = value["atk"][:bonus]
+    max_bonus = value["atk"][:bonus]    
     weapons_max.merge!("highest_bonus" => {"name" => key, "amount" => max_bonus})
   end
 end
 
-#Example
-puts weapons_output["Lucerne"]
-puts " "
-puts weapons_max.inspect
+for x in weapons_output
+ puts x
+ puts " "
+end
+
+puts weapons_max
