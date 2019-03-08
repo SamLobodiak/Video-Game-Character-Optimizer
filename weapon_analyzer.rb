791edb27c918e308a3bebfe208e3eb6bea20cb86
Samuel <samuel.lobodiak@gmail.com>
2019-03-08 14:26:47 -0600

    Initiate new hash with weapon max's

--- weapon_analyzer.rb
+++ weapon_analyzer.rb
@@ -25,7 +25,30 @@ for x in weapons
   weapons_output.merge!(x[:name] => {"atk" => x[:atk], "weight" => x[:weight], "reqs_sum" => reqs_sum, "to_fast_roll" => leftover_weight_for_armour_to_fast_roll, "to_med_roll" => leftover_weight_for_armour_to_med_roll, "to_slow_roll" => leftover_weight_for_armour_to_fat_roll})
 end
 
-puts " "
+weapons_max = Hash.new
+max_damage = 0
+max_bonus = 0
+max_magic = 0
+# This code block runs find max algorithms on the weapons and puts the maxes on a new hash call 'weapons_max'
+for key, value in weapons_output
+  #Max physical damage onto a hash
+  if value["atk"][:physical] > max_damage
+    max_damage = value["atk"][:physical]
+    weapons_max.merge!("highest_damage_physical" => { "name" => key, "amount" => max_damage})
+  end
+    #Max magic damage and putting it onto hash
+  if value["atk"][:magic] > max_magic
+    max_magic = value["atk"][:magic]
+    weapons_max.merge!("highest_damage_magic" => {"name" => key, "amount" => max_magic})
+  end
+  #Max bonus (critical hit multiplier) and putting it onto a hash
+  if value["atk"][:bonus] > max_bonus
+    max_bonus = value["atk"][:bonus]
+    weapons_max.merge!("highest_bonus" => {"name" => key, "amount" => max_bonus})
+  end
+end
 
 #Example
 puts weapons_output["Lucerne"]
+puts " "
+puts weapons_max.inspect
